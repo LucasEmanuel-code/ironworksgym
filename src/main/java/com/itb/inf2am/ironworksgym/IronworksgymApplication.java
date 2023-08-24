f09cc21b445750215d1ca883451b26cfd1395923
Lucas Emanuel Goncalves Oliveira <rm86247@estudante.fieb.edu.br>
2023-08-24 08:18:08 -0300

    Criação ambiente de desenvolvimento

---
+++ src/main/java/com/itb/inf2am/ironworksgym/IronworksgymApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2am.ironworksgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronworksgymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronworksgymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/inf2am/ironworksgym/IronworksgymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2am.ironworksgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronworksgymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
